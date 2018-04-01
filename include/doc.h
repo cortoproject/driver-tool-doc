d85c0bb63cafaf8d618d56cb3837e92fd3abff30
Sander Mertens <sander.mertens8@gmail.com>
2018-04-01 16:23:19 -0700

    Initial commit

---
+++ include/doc.h
@@ -0,0 +1,14 @@
+/* doc.h
+ *
+ * This is the main package file. Include this file in other projects.
+ * Only modify inside the header-end and body-end sections.
+ */
+
+#ifndef DRIVER_TOOL_DOC_H
+#define DRIVER_TOOL_DOC_H
+
+#include <corto/corto.h>
+#include <corto/c/c.h>
+
+
+#endif
